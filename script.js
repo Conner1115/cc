ddca53521765a0fec4b7bd654615a089a36242a1
LeviathanProgramming <connerow1115@gmail.com>
2021-02-12 10:03:30 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const $ = (prop, all) => {
+  if(!all)
+    return document.querySelector(prop);
+  else
+    return document.querySelectorAll(prop);
+}
+function animateElement(element, animation){
+  $(element).style.animation = animation;
+  $(element).style.animationFillMode = "forwards";
+}
+function cssVar(Var, Val){
+  $(":root").style.setProperty(Var, Val);
+}
+
+
+
+function setItems(){
+  if(window.innerWidth <= 600){
+    $("#nav-links").classList.add("nav-out");
+  }else{
+    $("#nav-links").classList = "";
+  }
+}
+window.onload = function(){
+  setItems();
+}
+window.onresize = setItems;
+
+
+
+function showNav(){
+  $("#nav-links").classList.add("nav-in");
+  $("#nav-links").classList.remove("nav-out");
+}
+function hideNav(){
+  $("#nav-links").classList.add("nav-out");
+  $("#nav-links").classList.remove("nav-in");
+}
